8e19ce47018e4559762aeb177eccbf7a52824cf6
chenzhi <chenzhi@qq.com>
2022-03-29 16:40:41 +0900

    说明：T 和 Any 的作用 和区别

--- app/src/main/java/com/example/testkotlin/TemplateActivity.kt
+++ app/src/main/java/com/example/testkotlin/TemplateActivity.kt
@@ -13,12 +13,12 @@ class TemplateActivity : AppCompatActivity() {
         binding = ActivityTemplateBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.normal.setOnClickListener { 普通泛型的例子() }
-        binding.addNumbersTest.setOnClickListener { addNumbersTest() }
+        binding.normal.setOnClickListener { test_T() }
+        binding.addNumbersTest.setOnClickListener { test_fun_T() }
 
-        binding.test2.setOnClickListener { Log.e(TAG, "泛型函数_函数内指定返回的类型: " + 泛型函数_函数内指定返回的类型(10)) }
+        binding.test2.setOnClickListener { Log.e(TAG, "T函数_返回为指定类型的函数: " + fun_T_return_other_T(10)) }
 
-        binding.test3.setOnClickListener { 泛型函数_函数内指定返回的类型_函数内(123.456) }
+        binding.test3.setOnClickListener { fun_T_return_other_T_inFun(123.456) }
 
         binding.testAny.setOnClickListener {
             泛型函数_any(123)
@@ -52,12 +52,12 @@ class TemplateActivity : AppCompatActivity() {
     }
 
     //泛型函数_函数内指定返回的类型_返回值
-    fun <T> 泛型函数_函数内指定返回的类型(t: T): Int {
+    fun <T> fun_T_return_other_T(t: T): Int {
         return t.toString().toInt()
     }
 
     //泛型函数_函数内指定返回的类型_函数内
-    fun <T> 泛型函数_函数内指定返回的类型_函数内(t: T) {
+    fun <T> fun_T_return_other_T_inFun(t: T) {
         Log.e(TAG, "泛型函数_函数内指定返回的类型: " + t.toString().toDouble())
     }
 
@@ -67,28 +67,33 @@ class TemplateActivity : AppCompatActivity() {
     }
 
     //泛型函数 begin
-    fun <T> addNumbers(num1: T, num2: T): T {
+    fun <T> fun_T(num1: T, num2: T): T {
         return (num1.toString().toDouble() + num2.toString().toDouble()) as T
     }
 
-    fun addNumbersTest() {
-        Log.e(TAG, "addNumbersTest: " + addNumbers(10, 20))      // 結果: 30
-        Log.e(TAG, "addNumbersTest: " + addNumbers(10.1, 20.1))  // 結果: 30.200000000000003
+    fun test_fun_T() {
+        Log.e(TAG, "addNumbersTest: " + fun_T(10, 20))      // 結果: 30
+        Log.e(TAG, "addNumbersTest: " + fun_T(10.1, 20.1))  // 結果: 30.200000000000003
     }
     //泛型函数 end
 
-    private fun 普通泛型的例子() {
+    private fun test_T() {
         class Box<T>(val contents: T)
 
         val x = Box("a")
         val y = Box(0)
         val z = Box(listOf(1, 2, 3))
 
+
         Log.e(TAG, "contents: " + x.contents + "bc")   //abc
         Log.e(TAG, "contents: " + y.contents + 1)     //1
         z.contents.forEach {
             Log.e(TAG, "contents: " + it)
         }   //123
+
+
+        val testUnit = Box(Unit)
+        Log.e(TAG, "testUnit: " + testUnit.contents )   //kotlin.Unit
     }
 
     companion object {
